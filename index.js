369f98d2cdd15c1b1c3e9c761bcae2e750ffbed7
Alexandre Abita <xouabita@gmail.com>
2018-01-25 12:30:14 +0100

    Add histogram functionalities

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const execa = require("execa")
 const path = require("path")
 const a = require("awaiting")
+const {Database, Model} = require("mongorito")
 
 const socket = require("socket.io-client")("https://domopets.herokuapp.com/")
 
 const hx711Path = path.join(__dirname, "..", "hx711py")
 const tare_cmd = path.join(hx711Path, "tare")
 const measure_cmd = path.join(hx711Path, "measure")
+
+const db = new Database("localhost/weight")
+class Weight extends Model {}
 
 async function tare() {
   const {stdout} = await execa(tare_cmd)
@@ -18,14 +22,56 @@ async function measure(tare) {
   return parseInt(stdout)
 }
 
+function getMonday() {
+  const d = new Date()
+  var day = d.getDay(),
+    diff = d.getDate() - day + (day == 0 ? -6 : 1)
+  return new Date(d.setDate(diff))
+}
+
+async function getHistogram() {
+  const ten = (await Weight.sort({date: -1}).limit(10).find())
+    .map(w => w.get("value"))
+    .reverse()
+  const unique = ten.reduce((a, b) => a + b) / ten.length
+  const collection = await Weight.getCollection()
+  collection
+    .aggregate([
+      {$match: {date: {$gte: getMonday()}}},
+      {$project: {dayOfWeek: {$dayOfWeek: "$date"}}},
+      {$group: {_id: "$dayOfWeek", count: {$sum: 1}}},
+    ])
+    .toArray((err, doc) => {
+      const days = {}
+      const week = []
+      doc.forEach(d => (days[d._id - 2] = d.count))
+      for (let i = 0; i < 7; i++) {
+        week[i] = days[i] || 0
+      }
+      socket.emit("dispatch", {
+        action: "histogram",
+        payload: {
+          unique,
+          ten,
+          week,
+        },
+      })
+    })
+}
+
+async function start() {
+  await db.connect()
+  db.register(Weight)
+  socket.on("histogram", getHistogram)
+}
+
 let tareVal
 let tareTriggered = false
 let catOnLitter = false
-let countOnLitter = 0
-let weightOnLitter = 0
 let lastVal = 0
+let weights = []
 
-tare().then(async val => {
+start().then(() => tare()).then(async val => {
   tareVal = val
   const measureLoop = async () => {
     if (tareTriggered) {
@@ -34,18 +80,24 @@ tare().then(async val => {
       tareTriggered = false
     }
     const measureValue = await measure(tareVal)
-    if (Math.abs(lastVal - measureValue) > 700) {
+    if (Math.abs(lastVal - measureValue) > 400) {
       catOnLitter = !catOnLitter
       if (!catOnLitter) {
-        socket.emit("catOnLitter", weightOnLitter / countOnLitter)
-        weightOnLitter = 0
-        countOnLitter = 0
+        if (weights.length > 5) {
+          const total = weights.reduce((a, b) => a + b)
+          const weight = new Weight({
+            value: total / weights.length,
+            date: new Date(),
+          })
+          await weight.save()
+          getHistogram()
+        }
+        weights = []
       }
     }
 
     if (catOnLitter) {
-      weightOnLitter += measureValue
-      countOnLitter++
+      weights.push(measureValue)
     }
 
     lastVal = measureValue
